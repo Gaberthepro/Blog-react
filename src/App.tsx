c898a5e39ab5c0be0f6921bdd9840ed804e97e23
GaberČuješ <gabercujes@gmail.com>
2024-03-25 08:00:55 +0100

    adding Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar'
 
 function App() {
   return (
     <div className="App">
-      <h1>Blog</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
